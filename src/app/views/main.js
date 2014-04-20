9fff004f67764b3ac52f1db0e4451d5e0e8acbba
Germán Mauricio Muñoz <gmauricio.munoz@gmail.com>
2014-04-20 12:36:08 -0500

    Improved styling for connected users area

--- src/app/views/main.js
+++ src/app/views/main.js
@@ -155,7 +155,7 @@ define('app/views/main',[
 
       toggleUsers: function(e){
           e.preventDefault();
-          $("#online_users_container").toggle("slow");
+          $('#sidebar').toggleClass('active');
       }
   });
   return MainView;
